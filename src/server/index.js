20ea112b03c177a7020f8569ca2e4257b3e66e28
Eddie <elremolino91@gmail.com>
2021-11-29 22:10:39 -0800

    set up mongodb connection

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,35 @@
-const express = require('express')
-const app = express()
+const express = require('express');
 const path = require('path');
+const mongodb = require('mongodb');
+const dotenv = require('dotenv');
 
-const port = 3000
+dotenv.config()
+const MongoClient = mongodb.MongoClient;
 
-app.use(express.urlencoded({ extended: true }));
-app.use('/', express.static(path.join(__dirname, './client')));
+const app = express();
+const port = process.env.PORT || 8000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+MongoClient.connect(
+  process.env.FREE_DB_URI,
+  {
+    poolSize: 50,
+    wtimeout:2500,
+    useNewUrlParse: true
+  }
+  )
+  .catch(err => {
+    consol.error(err.stack);
+    process.exit(1);
+  })
+  .then(async client => {
+    app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`)
+    })
+  })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// app.use(express.urlencoded({ extended: true }));
+// app.use('/', express.static(path.join(__dirname, './client')));
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
