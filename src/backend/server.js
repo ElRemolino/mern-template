31b572e12259f2677662e498af6374ae49ebcc1c
Eddie <elremolino91@gmail.com>
2021-11-29 23:54:53 -0800

    mongodb successfully connected

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
+
 const PORT = 3000;
 
+mongoose.connect('mongodb://localhost:27017/Tutorial')
+  .catch(() => console.error('error connecting with DB'))
+  .then(()=>{
+    console.log("DB Connected Successfully");
+});
 
 app.listen(PORT,()=>{
     console.log(`Listening on port ${PORT}`);
