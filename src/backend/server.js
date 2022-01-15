bb9cd3f18a8f297c076a0ad8fc1489cb3b2413c3
Eddie <elremolino91@gmail.com>
2022-01-14 17:54:33 -0800

    added middleware to serve dist folder

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const mongoose = require('mongoose');
 
 const PORT = 3000;
@@ -9,6 +10,8 @@ mongoose.connect('mongodb://localhost:27017/Tutorial')
   .then(()=>{
     console.log("DB Connected Successfully");
 });
+
+app.use('/', express.static(path.join(__dirname, '../../dist')));
 
 app.listen(PORT,()=>{
     console.log(`Listening on port ${PORT}`);
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.join(__dirname, "src", "index.js"),
+  entry: path.join(__dirname, "src/client", "index.js"),
   output: {
     path: path.join(__dirname, "dist"),
     filename: "bundle.js"
@@ -31,7 +31,7 @@ module.exports = {
   },
   plugins: [
       new HtmlWebpackPlugin({
-          template: path.join(__dirname, "src", "index.html"),
+          template: path.join(__dirname, "src/client", "index.html"),
       }),
   ],
 };
